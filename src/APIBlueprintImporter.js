761d0a1ff40a90d7557c8c093b0646db7e91f13e
Kyle Fuller <kyle@fuller.li>
2019-06-04 18:45:43 +0100

    refactor: remove drafter.js
    drafter.js was used for the sync API, as Paw now supports returning
    promises we can use async/await with Fury.
    
    Removing drafter.js simplifies package management as the dependency is
    also pulled in via fury-adapter-apib-parser.

--- src/APIBlueprintImporter.js
+++ src/APIBlueprintImporter.js
@@ -1,7 +1,18 @@
 import {Fury} from 'fury';
-import {detect} from 'fury-adapter-apib-parser';
-import {parseSync} from 'drafter.js';
+import APIBlueprintParser from 'fury-adapter-apib-parser';
 import APIElementImporter from './APIElementImporter.js'
+
+function promisify(func, thisArg) {
+  return (...args) => new Promise((resolve, reject) => {
+    func.bind(thisArg)(...args, (err, result) => {
+      if (result) {
+        resolve(result);
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
 
 @registerImporter
 export default class APIBlueprintImporter {
@@ -13,11 +24,12 @@ export default class APIBlueprintImporter {
 
   constructor() {
     this.fury = new Fury();
+    this.fury.use(APIBlueprintParser);
   }
 
   canImport(context, items) {
     for (const item of items) {
-      if (detect(item.content)) {
+      if (APIBlueprintParser.detect(item.content)) {
         return true
       }
     }
@@ -25,12 +37,15 @@ export default class APIBlueprintImporter {
     return false;
   }
 
-  import(context, items, options) {
+  async import(context, items, options) {
     const defaultHost = options.inputs['host'];
+
+    // FIXME remove promisify when https://github.com/apiaryio/api-elements.js/issues/37 is live
+    const parse = promisify(this.fury.parse, this.fury);
 
     for (const item of items) {
       console.log('Importing Item');
-      const parseResult = this.fury.load(parseSync(item.content));
+      const parseResult = await parse({ source: item.content, mediaType: 'text/vnd.apiblueprint' });
       const importer = new APIElementImporter(context, defaultHost);
       importer.importAPI(parseResult.api);
     }
